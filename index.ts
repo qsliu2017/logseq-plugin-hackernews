36a5aa67faa886414e6e7b2b911a304a466d1250
qsliu <qsliu2017@outlook.com>
2023-03-26 13:29:31 +0800

    setting: maxTopStories & template

--- index.ts
+++ index.ts
@@ -1,20 +1,41 @@
 import "@logseq/libs";
+import { SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin.user";
 import { HackerNewsAPI } from "./api";
+
+const settings: SettingSchemaDesc[] = [
+  {
+    title: "Max Top Stories",
+    key: "maxTopStories",
+    description: "Max number of top stories to fetch",
+    type: "number",
+    default: 10,
+  },
+  {
+    title: "Template",
+    key: "template",
+    description: "Template for each item",
+    type: "string",
+    default: "[{{title}}]({{url}})",
+  },
+];
 
 function main() {
   logseq.Editor.registerSlashCommand(
     'hackernews-topstories',
     async () => {
       const { uuid } = (await logseq.Editor.getCurrentBlock())!;
+      const maxTopStories: number = logseq.settings!["maxTopStories"];
+      const template: string = logseq.settings!["template"];
+
       const api = new HackerNewsAPI();
-      const itemIds = (await api.getTopStories()).slice(0, 10);
+      const itemIds = (await api.getTopStories()).slice(0, maxTopStories);
       itemIds.forEach(
         async (id) => {
           const item = await api.getItem(id);
           const { title, url } = item;
           logseq.Editor.insertBlock(
             uuid,
-            `[${title}](${url})`,
+            template.replace("{{title}}", title!).replace("{{url}}", url!),
           );
         }
       )
@@ -23,5 +44,6 @@ function main() {
 }
 
 logseq
+  .useSettingsSchema(settings)
   .ready(main)
   .catch(console.error);
