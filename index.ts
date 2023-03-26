17c8ae1b5fd0601b91203b394348133fc459ef9f
qsliu <qsliu2017@outlook.com>
2023-03-26 16:16:23 +0800

    beta: gpt-3.5-turbo is too short for long html

--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
 import "@logseq/libs";
 import { SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin.user";
+import { Configuration, OpenAIApi } from "openai";
 import { HackerNewsAPI } from "./api";
+import htmlToText from "html2text";
+
+const openaiSettings: SettingSchemaDesc[] = [
+  {
+    title: "OpenAI API Key",
+    key: "apiKey",
+    description: "OpenAI API Key",
+    type: "string",
+    default: "",
+  },
+  {
+    title: "OpenAI Model",
+    key: "model",
+    description: "OpenAI Model",
+    type: "string",
+    default: "gpt-3.5-turbo",
+  },
+];
 
 const settings: SettingSchemaDesc[] = [
   {
@@ -17,6 +36,14 @@ const settings: SettingSchemaDesc[] = [
     type: "string",
     default: "[{{title}}]({{url}})",
   },
+  {
+    title: "Summarize",
+    key: "summarize",
+    description: "Summarize the top stories",
+    type: "boolean",
+    default: false,
+  },
+  ...openaiSettings,
 ];
 
 function main() {
@@ -26,6 +53,10 @@ function main() {
       const { uuid } = (await logseq.Editor.getCurrentBlock())!;
       const maxTopStories: number = logseq.settings!["maxTopStories"];
       const template: string = logseq.settings!["template"];
+      const summarize: boolean = logseq.settings!["summarize"];
+      const apiKey: string = logseq.settings!["apiKey"];
+      const model: string = logseq.settings!["model"];
+      const openai = new OpenAIApi(new Configuration({ apiKey }));
 
       const api = new HackerNewsAPI();
       const itemIds = (await api.getTopStories()).slice(0, maxTopStories);
@@ -33,10 +64,29 @@ function main() {
         async (id) => {
           const item = await api.getItem(id);
           const { title, url } = item;
-          logseq.Editor.insertBlock(
+          const uuid_ = (await logseq.Editor.insertBlock(
             uuid,
             template.replace("{{title}}", title!).replace("{{url}}", url!),
-          );
+          ))!.uuid;
+          if (summarize) {
+            const html = await (await fetch(url!)).text();
+            const text = htmlToText.fromString(html);
+            // logseq.Editor.insertBlock(
+            //   uuid_,
+            //   text,
+            // )
+            const resp = await openai.createChatCompletion({
+              messages: [{ "role": "user", "content": text + `\n---\nSummarize` }],
+              model: model,
+            })
+            const summary = resp.data.choices[0].message;
+            if (summary) {
+              logseq.Editor.insertBlock(
+                uuid_,
+                summary.content,
+              )
+            }
+          }
         }
       )
     },
@@ -46,4 +96,4 @@ function main() {
 logseq
   .useSettingsSchema(settings)
   .ready(main)
-  .catch(console.error);
+  .catch(logseq.UI.showMsg);
